42232423633cb80cb2558571bc0ae28d9264244d
Theo Kanning <theokanning@gmail.com>
2018-03-17 10:04:53 -0500

    Add Point class and sample image

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Python-RRT
 A simple, non-optimized implementation of the Rapidly-Exploring Random Trees (RRT) algorithm in python
 
+![Results](images/screenshot.PNG)
+
 ## Running
 ```
 python rrt.py
--- rrt.py
+++ rrt.py
@@ -4,26 +4,27 @@ from math import sqrt
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+Point = namedtuple('Point', 'x y')
 Node = namedtuple('Node', 'id pos parent')
 Region = namedtuple('Region', 'pos width height')  # bottom left x,y
 
 WORLD_SIZE = 100
 GOAL_CHANCE = 0.01
-velocity = 2
+velocity = 1
 
 
 def dist(point1, point2):
-    return sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+    return sqrt((point1[0] - point2[0]) ** 2 + (point1.y - point2.y) ** 2)
 
 
 def in_region(point, region):
     return region.pos[0] <= point[0] <= region.pos[0] + region.width \
-           and region.pos[1] <= point[1] <= region.pos[1] + region.height
+           and region.pos.y <= point.y <= region.pos.y + region.height
 
 
-def in_regions(point, obstacles):
-    for obstacle in obstacles:
-        if in_region(point, obstacle):
+def in_regions(point, regions):
+    for region in regions:
+        if in_region(point, region):
             return True
     return False
 
@@ -39,19 +40,19 @@ def steer(point1, point2):
     """
     Return an intermediate point on the line between point1 and point2
     """
-    total_offset = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
-    x = point1[0] + (point2[0] - point1[0]) / total_offset
-    y = point1[1] + (point2[1] - point1[1]) / total_offset
-    return x, y
+    total_offset = abs(point2.x - point1.x) + abs(point2.y - point1.y)
+    x = point1.x + velocity * ((point2.x - point1.x) / total_offset)
+    y = point1.y + velocity * ((point2.y - point1.y) / total_offset)
+    return Point(x, y)
 
 
 def calculate_path(start, goal, obstacles):
     nodes = [Node(0, start, 0)]
     while True:
         if uniform(0, 1) < GOAL_CHANCE:
-            z_rand = (goal.pos[0] + goal.width / 2, goal.pos[1] + goal.height / 2)
+            z_rand = Point(goal.pos.x + goal.width / 2, goal.pos.y + goal.height / 2)
         else:
-            z_rand = (randint(0, WORLD_SIZE), randint(0, WORLD_SIZE))
+            z_rand = Point(randint(0, WORLD_SIZE), randint(0, WORLD_SIZE))
         if in_regions(z_rand, obstacles):
             continue
         nearest = get_closest(nodes, z_rand)
@@ -71,11 +72,11 @@ def calculate_path(start, goal, obstacles):
 
 
 if __name__ == '__main__':
-    start_pos = (WORLD_SIZE / 4, WORLD_SIZE / 4)
-    end_region = Region((3 * WORLD_SIZE / 4, 3 * WORLD_SIZE / 4), 10, 10)
+    start_pos = Point(WORLD_SIZE / 4, WORLD_SIZE / 4)
+    end_region = Region(Point(3 * WORLD_SIZE / 4, 3 * WORLD_SIZE / 4), 10, 10)
     obstacles = [
-        Region([0, 30], 80, 10),
-        Region([20, 60], 80, 10)
+        Region(Point(0, 30), 80, 10),
+        Region(Point(20, 60), 80, 10)
     ]
     nodes = calculate_path(start_pos, end_region, obstacles)
     node_count = len(nodes)
@@ -87,12 +88,12 @@ if __name__ == '__main__':
         axes = fig.add_subplot(subplot, aspect='equal')
         for node in nodes:
             parent = nodes[node.parent]
-            plt.plot([node.pos[0], parent.pos[0]], [node.pos[1], parent.pos[1]], 'k-')
+            plt.plot([node.pos.x, parent.pos.x], [node.pos.y, parent.pos.y], 'k-')
 
         for obstacle in obstacles:
             axes.add_patch(patches.Rectangle(obstacle.pos, obstacle.width, obstacle.height, color=(0, 0, 0, .3)))
 
-        axes.plot(start_pos[0], start_pos[1], 'ro')
+        axes.plot(start_pos.x, start_pos.y, 'ro')
         axes.add_patch(patches.Rectangle(end_region.pos, end_region.width, end_region.height, color='g'))
         axes.set_title("{} Points".format(len(nodes)))
         axes.set_xlim([0, WORLD_SIZE])
@@ -109,7 +110,7 @@ if __name__ == '__main__':
     current_node = nodes[-1]
     while current_node.id != 0:
         parent = nodes[current_node.parent]
-        plt.plot([current_node.pos[0], parent.pos[0]], [current_node.pos[1], parent.pos[1]], 'b-', linewidth=2.0)
+        plt.plot([current_node.pos.x, parent.pos.x], [current_node.pos.y, parent.pos.y], 'b-', linewidth=2.0)
         current_node = parent
 
     plt.show()
